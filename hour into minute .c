2951c483e6dfded3c66893fb3518624e6e38e7aa
riyabhuva2007-coder <riyabhuva2007@gmail.com>
2025-08-26 23:05:42 +0530

    Create hour into minute .c

---
+++ hour into minute .c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    int minutes;
+    int hours;
+
+    printf("Enter hour: ");
+    scanf("%d", &hours);
+
+    hours = hours / 60;
+
+    printf("minutes: %d\n", minutes);
+
+    return 0;
+}
